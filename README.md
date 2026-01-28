75d394c4ac4941bf6b317d3e6831202bc137f2f6
Dominik Schilling <dominikschilling+git@gmail.com>
2026-01-28 15:14:11 +0100

    Update plugin metadata and installation instructions

--- README.md
+++ README.md
@@ -3,8 +3,7 @@ WP Migrate DB Anonymization Addon
 
 ## Installation
 
-If you clone from this repository you will need to run `composer install` to complete the install of the plugin. The `vendor` directory is packaged with the plugin on the WordPress plugin repository:
-https://wordpress.org/plugins/wp-migrate-db-anonymization/#developers 
+    composer require wearerequired/wp-migrate-db-anonymization
 
 The addon should be installed on the live site. This ensures any time the site's database is exported, pushed or pulled, the user data is anonymized.
 
@@ -20,7 +19,7 @@ These constants should be defined in the site's wp-config.php file, for example:
 
     define( 'WPMDB_ANONYMIZATION_USER_LOGIN_WHITELIST', 'somelogin, anotherloginname' );
     define( 'WPMDB_ANONYMIZATION_DEFAULT_PASSWORD', 'password123' );
-    
+
 You can also programmatically control which users are whitelisted with the following filter, added to a plugin or mu-plugin file:
 
     /**
@@ -34,11 +33,11 @@ You can also programmatically control which users are whitelisted with the follo
             // All users with the login containing an email address of somedomain.com are whitelisted
             return true;
         }
-        
+
         return $whitelisted;
     }
     add_filter( 'wpmdb_anonymization_user_whitelisted', 'my_wpmdb_anonymization_user_whitelisted', 10, 2 );
-    
+
 ## Extending
 
 The rules for anonymization can be extended using the `wpmdb_anonymization_config` filter, added to a plugin or mu-plugin file:
@@ -58,7 +57,7 @@ The rules for anonymization can be extended using the `wpmdb_anonymization_confi
 
         return $config;
     }
-    
+
     add_filter( 'wpmdb_anonymization_config', 'my_wpmdb_anonymization_rules' );
 
 If you need to pass arguments to the Faker data method then you can define this using the `fake_data_args` key:
@@ -79,5 +78,5 @@ If you need to pass arguments to the Faker data method then you can define this 
 
         return $config;
     }
-    
+
     add_filter( 'wpmdb_anonymization_config', 'my_wpmdb_anonymization_rules' );
--- wp-migrate-db-anonymization.php
+++ wp-migrate-db-anonymization.php
@@ -1,24 +1,35 @@
 <?php
-/*
-Plugin Name: WP Migrate Anonymization
-Plugin URI: https://deliciousbrains.com/wp-migrate-db-pro/
-Description: An extension to WP Migrate and WP Migrate Pro that anonymizes user data.
-Author: Delicious Brains
-Version: 0.3.4
-Author URI: https://deliciousbrains.com
-Network: True
-*/
-
-// Copyright (c) 2018 Delicious Brains. All rights reserved.
-//
-// Released under the GPL license
-// http://www.opensource.org/licenses/gpl-license.php
-//
-// **********************************************************************
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// **********************************************************************
+/**
+ * Plugin Name: WP Migrate Anonymization
+ * Plugin URI: https://github.com/wearerequired/wp-migrate-db-anonymization
+ * Description: An extension to WP Migrate and WP Migrate Pro that anonymizes user data.
+ * Version: 0.4.0
+ * Requires at least: 6.8
+ * Requires PHP: 8.3
+ * Author: required
+ * Author URI: https://required.com
+ * Update URI: false
+ * Text Domain: wp-migrate-db-anonymization
+ * License: GPL v3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * Copyright (c) 2018 Delicious Brains. All rights reserved.
+ * Copyright (c) 2026 required (email: info@required.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -35,7 +46,7 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
  * instance to functions everywhere.
  */
 function wpmdb_anonymize() {
-	$version = '0.3.4';
+	$version = '0.4.0';
 	return WPMDB\Anonymization\Plugin::get_instance( __FILE__, $version );
 }
 
